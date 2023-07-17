ced6608b02a6bededb2adde0f84e6a76f9c61266
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-17 10:22:41 +0800

    [User Login] - Create a window

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 # BSCPE 1-5
 # Creating a program that allows the user to input information that will be used for Covid-19 tracing
 
+
+
 # PSEUDOCODE
 
+# LET USER LOG IN OR SIGN UP
 # LET USER INPUT NEEDED DETAILS
 # SAVE INFORMATION OF USER IN A FILE
 # CREATE SEARCH BUTTON
---
+++ user_login.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+class User_Login:
+    def login ():
+        pass
+
+window = Tk ()
+window.title("User Login")
+window.geometry("1000x500")
+window.config(bg="gray")
+window.mainloop()

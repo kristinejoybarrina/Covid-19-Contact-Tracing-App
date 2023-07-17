3fc0fe70feeecf2cd6b59d2ec26d7d6783f8f53b
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-17 11:10:59 +0800

    [User Login] - Put background image in window

--- user_login.py
+++ user_login.py
@@ -1,12 +1,30 @@
 from tkinter import *
-
+from PIL import Image, ImageTk
 
 class User_Login:
-    def login ():
+    def login():
         pass
 
-window = Tk ()
-window.title("User Login")
+
+window = Tk()
+window.title("Trace Corona")
 window.geometry("1000x500")
-window.config(bg="gray")
+window.config(bg="white")
+window.resizable(False, False)
+
+# Open the image
+image = Image.open("background.png")
+
+# Resize the image to fit the window proportionally
+window_width = 750
+window_height = 500
+image = image.resize((window_width, window_height), Image.ANTIALIAS)
+
+# Convert the image to a Tkinter-compatible object
+background = ImageTk.PhotoImage(image)
+
+# Create a label with the resized image
+label_background = Label(window, image=background, bg="white")
+label_background.place(x=-2, y=-2)
+
 window.mainloop()

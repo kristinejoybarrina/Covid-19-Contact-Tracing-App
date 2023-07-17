db25b8efe605eec95d1cbcd093e9f00457280be7
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-17 11:18:43 +0800

    [User Login] - Put all window codes inside a def function

--- user_login.py
+++ user_login.py
@@ -2,29 +2,34 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 class User_Login:
+
+    def main():
+
+        window = Tk()
+        window.title("Trace Corona")
+        window.geometry("1000x500")
+        window.config(bg="white")
+        window.resizable(False, False)
+
+        # Open the image from files
+        image = Image.open("background.png")
+
+        # Resize the image to fit the window proportionally
+        window_width = 750
+        window_height = 500
+        image = image.resize((window_width, window_height), Image.ANTIALIAS)
+
+        # Convert the image to a Tkinter-compatible object
+        background = ImageTk.PhotoImage(image)
+
+        # Create a label with the resized image in window
+        label_background = Label(window, image=background, bg="white")
+        label_background.place(x=-2, y=-2)
+
+        window.mainloop()
+
+    # Call main to start window
+    main()
+
     def login():
-        pass
-
-
-window = Tk()
-window.title("Trace Corona")
-window.geometry("1000x500")
-window.config(bg="white")
-window.resizable(False, False)
-
-# Open the image
-image = Image.open("background.png")
-
-# Resize the image to fit the window proportionally
-window_width = 750
-window_height = 500
-image = image.resize((window_width, window_height), Image.ANTIALIAS)
-
-# Convert the image to a Tkinter-compatible object
-background = ImageTk.PhotoImage(image)
-
-# Create a label with the resized image
-label_background = Label(window, image=background, bg="white")
-label_background.place(x=-2, y=-2)
-
-window.mainloop()
+        username = username_entry.get()

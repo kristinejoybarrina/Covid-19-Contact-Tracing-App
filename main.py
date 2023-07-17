a299c82b2cea9c734e74e80881758022c1142822
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-17 09:21:26 +0800

    Pseudocode

---
+++ main.py
@@ -0,0 +1,9 @@
+# Kristine Joy Barrina
+# BSCPE 1-5
+# Creating a program that allows the user to input information that will be used for Covid-19 tracing
+
+# PSEUDOCODE
+
+# LET USER INPUT NEEDED DETAILS
+# SAVE INFORMATION OF USER IN A FILE
+# CREATE SEARCH BUTTON

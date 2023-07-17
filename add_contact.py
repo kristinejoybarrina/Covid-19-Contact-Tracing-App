2ca8b5ded2fe4c3be11cf015f76d1d8b02a7208c
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-17 18:36:31 +0800

    [Add Contact] - Create add contact def function for main window

--- add_contact.py
+++ add_contact.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter.font import Font
+
 
 class Add_Contact():
     
@@ -8,5 +10,11 @@ class Add_Contact():
         window.geometry("1000x500")
         window.config(bg="#f0f0f0")
         window.resizable(False,False)
+        
+        label_font = Font(family="Helvetica", size=15, weight="bold")
+        
+        # Create Labels
+        window_label1 = Label(window, text="Contact Tracing Information", fg="black", font=label_font)
+        window_label1.place(x=15, y=20) 
         
     

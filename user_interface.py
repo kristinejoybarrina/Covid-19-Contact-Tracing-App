637f04f9486c08f971ee6f5dceec56ca62c91f01
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-17 14:15:20 +0800

    [User Interface] - Design buttons using images

--- user_interface.py
+++ user_interface.py
@@ -8,7 +8,7 @@ class User_Interface:
         window = Tk()
         window.title("Trace Corona")
         window.geometry("1000x500")
-        window.config(bg="white")
+        window.config(bg="#f0f0f0")
         window.resizable(False,False)
 
         # Open the image from files
@@ -23,7 +23,7 @@ class User_Interface:
         background = ImageTk.PhotoImage(image)
 
         # Create a label with the resized image in window
-        label_background = Label(window, image=background, bg="white")
+        label_background = Label(window, image=background, bg="white", bd=0, borderwidth=0, highlightthickness=0)
         label_background.place(x=-2, y=-2)
 
         # Link the add_contact button
@@ -40,19 +40,41 @@ class User_Interface:
     # Create add contact button 
     def add_contact(window):
         
-        add_button = Button(window, text="ADD CONTACT")
-        add_button.place(x=835, y=200)
+        button_img = Image.open("add_contact_button.png")
+        button_png = ImageTk.PhotoImage(button_img)
+
+        
+        add_button = Button(window, image=button_png, border=0, borderwidth=0, command=User_Interface.check_if_working,)
+        add_button.image = button_png
+        add_button.place(x=803, y=180)
+        
+    def check_if_working():
+        print ("ITS WORKINGGG!!")
         
     # Create add contact button 
     def search_contact(window):
-        
-        search_button = Button(window, text="SEARCH CONTACT")
-        search_button.place(x=823, y=250) 
+
+        button_img = Image.open("search_contact_button.png")
+        button_png = ImageTk.PhotoImage(button_img)
+
+        search_button = Button(window, image=button_png, border=0, borderwidth=0, command=User_Interface.check_if_working,)
+        search_button.image = button_png
+        search_button.place(x=785, y=250) 
     
     # Create exit button 
     def exit_contact(window):
-        search_button = Button(window, text="EXIT", command=window.destroy)
-        search_button.place(x=860, y=300) 
+        
+        button_img = Image.open("exit_button.png")
+        button_png = ImageTk.PhotoImage(button_img)
+
+        exit_button = Button(window, image=button_png, border=0, borderwidth=0, command=window.destroy)
+        exit_button.image = button_png
+        exit_button.place(x=810, y=320) 
+        
+    
+        
+        
+
 
 
 

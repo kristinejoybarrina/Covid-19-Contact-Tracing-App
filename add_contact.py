7339fa66bcfe07acde428b73a44ffeeda7558539
kristinejoybarrina <barrinakristinejoy@gmail.com>
2023-07-24 14:08:18 +0800

    Revise position of buttons

--- add_contact.py
+++ add_contact.py
@@ -33,44 +33,49 @@ class AddContact():
         
     def name(window):
         
+        personal_info_label = Label(window, text="Personal Information", fg="black")
+        personal_info_label.place(x=50, y=65) 
+        
         # first name textbox
+        name_label = Label(window, text="Name", fg="black")
+        name_label.place(x=50, y=90) 
         first_name = Entry(window, fg="black", width=30, font=("Helvetica", 12), bg="#ECECEC")
-        first_name.place(x=50, y=100)
+        first_name.place(x=80, y=140)
         first_label = Label(window, text="First Name", fg="black")
-        first_label.place(x=50, y=70) 
+        first_label.place(x=80, y=110) 
         
         # middle name textbox
         middle_name = Entry(window, fg="black", width=30, font=("Helvetica", 12), bg="#ECECEC")
-        middle_name.place(x=50, y=155)
+        middle_name.place(x=80, y=195)
         middle_label = Label(window, text="Middle Name", fg="black")
-        middle_label.place(x=50, y=130) 
+        middle_label.place(x=80, y=170) 
         
         # surname textbox
         surname = Entry(window, fg="black", width=30, font=("Helvetica", 12), bg="#ECECEC")
-        surname.place(x=50, y=205)
+        surname.place(x=80, y=245)
         surname_label = Label(window, text="Surname", fg="black")
-        surname_label.place(x=50, y=183) 
+        surname_label.place(x=80, y=223) 
         
         # suffix textbox
         suffix = Entry(window, fg="black", width=30, font=("Helvetica", 12), bg="#ECECEC")
-        suffix.place(x=50, y=255)
+        suffix.place(x=80, y=295)
         suffix_label = Label(window, text="Suffix", fg="black")
-        suffix_label.place(x=50, y=233) 
+        suffix_label.place(x=80, y=273) 
         
     def email(window):
         
         # phone number textbox
         email = Entry(window, fg="black", width=30, font=("Helvetica", 12), bg="#ECECEC")
-        email.place(x=50, y=310)
+        email.place(x=50, y=350)
         email_label = Label(window, text="Email", fg="black")
-        email_label.place(x=50, y=285) 
+        email_label.place(x=50, y=325) 
 
     def phone_number(window):
         
         phone_number = Entry(window, fg="black", width=30, font=("Helvetica", 12), bg="#ECECEC")
-        phone_number.place(x=50, y=365)
+        phone_number.place(x=50, y=405)
         phone_number_label = Label(window, text="Phone Number", fg="black")
-        phone_number_label.place(x=50, y=340) 
+        phone_number_label.place(x=50, y=380) 
         
  
         

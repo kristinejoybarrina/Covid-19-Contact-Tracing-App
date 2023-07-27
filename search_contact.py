b289ed04e897000e605d3c06fb00a518b631137c
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-07-27 13:47:57 +0800

    [Search Contact] - Button that functioning properly but I already included phone search

--- search_contact.py
+++ search_contact.py
@@ -37,6 +37,9 @@ class SearchContact:
         canvas.create_window((0, 0), window=contact_frame, anchor="nw")
         contact_frame.bind("<Configure>", lambda e: canvas.config(scrollregion=canvas.bbox("all")))
         
+        self.__name_search_criteria = StringVar()
+        self.__phone_search_criteria = StringVar()
+        
         # Add the search bar and button for name search
         self.__name_search_label = Label(contact_frame, text="Search Name", fg="black", bg="#c3e7fd")
         self.__name_search_label.pack(anchor="w", padx=50, pady=15)
@@ -47,8 +50,16 @@ class SearchContact:
         self.__name_search_button = Button(contact_frame, text="Search", command=self.search_contact_by_name)
         self.__name_search_button.pack(anchor="w", padx=50, pady=5)
         
-        self.__name_search_criteria = StringVar()
         
+        # Add the search bar and button for phone number search
+        self.__phone_search_label = Label(contact_frame, text="Search Phone Number", fg="black", bg="#c3e7fd")
+        self.__phone_search_label.pack(anchor="w", padx=50, pady=15)
+        self.__phone_search_label = Label(contact_frame, text="Enter an 11-digits phone number starting with '09'", fg="black", bg="#c3e7fd")
+        self.__phone_search_label.pack(anchor="w", padx=50, pady=15)
+        self.__phone_search_entry = Entry(contact_frame, fg="black", width=50, font=("Helvetica", 12), bg="#f3faff", textvariable=self.__phone_search_criteria)
+        self.__phone_search_entry.pack(anchor="w", padx=50, pady=5)
+        self.__phone_search_button = Button(contact_frame, text="Search", command=self.search_contact_by_phone_number)
+        self.__phone_search_button.pack(anchor="w", padx=50, pady=5)
         
     def search_contact_by_name(self):
         search_criteria = self.__name_search_criteria.get()
@@ -67,11 +78,10 @@ class SearchContact:
         for entry in lines:
             if "Name:" in entry and search_criteria.lower() in entry.lower():
                 search_results.append(entry)
-                
+
         if not search_results:
             no_results_label = Label(self.__contact_frame, text="No matching contact found.", fg="black", bg="#c3e7fd")
             no_results_label.pack(anchor="w", padx=50, pady=5)
-            
             # Add a back button to return to search if it doesn't already exist
             if not hasattr(self, "__back_button"):
                 self.__back_button = Button(self.__contact_frame, text="Back to Search", command=self.go_back_to_search)
@@ -94,7 +104,13 @@ class SearchContact:
         self.__name_search_entry.pack(anchor="w", padx=50, pady=5)
         self.__name_search_button = Button(self.__contact_frame, text="Search", command=self.search_contact_by_name)
         self.__name_search_button.pack(anchor="w", padx=50, pady=5)
+        
+        # Add the back button again
+        if hasattr(self, "__back_button"):
+            self.__back_button.pack(anchor="w", padx=50, pady=5)
 
+        
+        self.__results_displayed = False
 
     def display_search_results(self, search_results):
         
